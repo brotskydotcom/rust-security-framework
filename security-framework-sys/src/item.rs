948855223987d07b62c98bb8fe4013df7acb3f7b
Daniel Brotsky <dan@brotsky.com>
2025-08-09 17:32:16 -0700

    Add ItemSearchOptions support for skipping authenticated items.
    Fixes kornelski/rust-security-framework#231.

--- security-framework-sys/src/item.rs
+++ security-framework-sys/src/item.rs
@@ -47,6 +47,10 @@ extern "C" {
     #[cfg(any(feature = "OSX_10_13", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
     pub static kSecUseAuthenticationContext: CFStringRef;
     #[cfg(any(feature = "OSX_10_13", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
+    pub static kSecUseAuthenticationUI: CFStringRef;
+    #[cfg(any(feature = "OSX_10_13", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
+    pub static kSecUseAuthenticationUISkip: CFStringRef;
+    #[cfg(any(feature = "OSX_10_13", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
     pub static kSecAttrSynchronizable: CFStringRef;
 
     pub static kSecAttrKeySizeInBits: CFStringRef;
